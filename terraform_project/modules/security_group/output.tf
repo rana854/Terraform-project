8eb13d16e83db74cdc57689a38320a210e50a8ca
rana tarek <132678372+rana854@users.noreply.github.com>
2024-03-19 22:08:43 +0200

    Update output.tf

--- terraform_project/modules/security_group/output.tf
+++ terraform_project/modules/security_group/output.tf
@@ -3,3 +3,13 @@ output "security_group_id" {
   description = "The ID of the created security group"
   value       = aws_security_group.allow_ssh.id
 }
+
+output "cidr_ingress_security_group" {
+  description = "CIDR block for ingress traffic in the security group"
+  value       = var.cidr_ingress_security_group
+}
+
+output "cidr_egress_security_group" {
+  description = "CIDR block for egress traffic in the security group"
+  value       = var.cidr_egress_security_group
+}
